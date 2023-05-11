3ef030de0e19e69d48527666bff48e1fcb6aeccd
Joel <joel.garcia.7e6@itb.cat>
2023-05-11 09:11:56 +0200

    Dialog Log out

--- app/src/main/java/com/example/geoquest_app/view/LogOutFragment.kt
+++ app/src/main/java/com/example/geoquest_app/view/LogOutFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.geoquest_app.R
 import com.example.geoquest_app.databinding.FragmentLogOutBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class LogOutFragment : Fragment() {
     private lateinit var binding: FragmentLogOutBinding
@@ -19,11 +21,20 @@ class LogOutFragment : Fragment() {
         return binding.root
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        // vaciar las prefs y userData.postValue(null)
-        val toLogin = LogOutFragmentDirections.actionLogOutFragmentToLogInFragment()
-        findNavController().navigate(toLogin)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        MaterialAlertDialogBuilder(requireContext())
+            .setMessage("Are you really sure you want to log out?")
+            .setPositiveButton("Yes, I'm sure") { dialog, which ->
+                val toLogin = LogOutFragmentDirections.actionLogOutFragmentToLogInFragment()
+                findNavController().navigate(toLogin)
+            }
+            .setNegativeButton("Take me back"){ dialog, which ->
+                dialog.dismiss()
+            }
+            .show()
     }
 
 }
